04db88055b47a482ddf968eafd0fbb9afe3f88da
cpp20120 <58726094+cpp20120@users.noreply.github.com>
2025-04-24 17:47:20 +0700

    Update build_all.ps1

--- build_all.ps1
+++ build_all.ps1
@@ -1,27 +1,16 @@
 # Create and build Debug
 New-Item -ItemType Directory -Path "build/debug" -Force
+Copy-Item "CMakePresets.json" -Destination "build/debug" -Force
 cd "build/debug"
-cmake -DCMAKE_BUILD_TYPE=Debug ..\..
-cmake --build .
-cd ..\..
+cmake ../.. --preset debug
+cmake --build --preset build-debug
+cd ../..
 
 # Create and build Release
 New-Item -ItemType Directory -Path "build/release" -Force
+Copy-Item "CMakePresets.json" -Destination "build/release" -Force
 cd "build/release"
-cmake -DCMAKE_BUILD_TYPE=Release ..\..
-cmake --build .
-cd ..\..
+cmake ../.. --preset release
+cmake --build --preset build-release
+cd ../..
 
-# Create and build RelWithDebInfo
-New-Item -ItemType Directory -Path "build/relwithdebinfo" -Force
-cd "build/relwithdebinfo"
-cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo ..\..
-cmake --build .
-cd ..\..
-
-# Create and build MinSizeRel
-New-Item -ItemType Directory -Path "build/minsizerel" -Force
-cd "build/minsizerel"
-cmake -DCMAKE_BUILD_TYPE=MinSizeRel ..\..
-cmake --build .
-cd ..\..
